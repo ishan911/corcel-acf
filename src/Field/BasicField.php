0e4df3f3da97d5b2f1eeae0c211586349bfa1680
ishanjayman <gt2ishan@gmail.com>
2024-01-21 17:32:27 +0530

    Updated to corcel 7

--- src/Field/BasicField.php
+++ src/Field/BasicField.php
@@ -7,6 +7,8 @@ use Corcel\Model;
 use Corcel\Model\Meta\PostMeta;
 use Corcel\Model\Term;
 use Corcel\Model\Meta\TermMeta;
+use Corcel\Model\User;
+use Corcel\Model\Meta\UserMeta;
 
 /**
  * Class BasicField.
@@ -63,6 +65,8 @@ abstract class BasicField
             $this->postMeta = new PostMeta();
         } elseif ($post instanceof Term) {
             $this->postMeta = new TermMeta();
+        } elseif ($post instanceof User) {
+            $this->postMeta = new UserMeta();
         }
 
         $this->postMeta->setConnection($post->getConnectionName());
@@ -78,7 +82,8 @@ abstract class BasicField
     public function fetchValue($field)
     {
         $postMeta = $this->postMeta->where(
-           $this->getKeyName(), $this->post->getKey()
+            $this->getKeyName(),
+            $this->post->getKey()
         )->where('meta_key', $field)->first();
 
         if (isset($postMeta->meta_value) and ! is_null($postMeta->meta_value)) {
@@ -151,6 +156,8 @@ abstract class BasicField
             return 'post_id';
         } elseif ($this->post instanceof Term) {
             return 'term_id';
+        } elseif ($this->post instanceof User) {
+            return 'user_id';
         }
     }
 
